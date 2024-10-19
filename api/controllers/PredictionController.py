d03f7dc2562b695107014eac626edd4d4e4750f5
Ahmad Nadil <13521024@std.stei.itb.ac.id>
2024-10-19 15:20:30 +0200

    fix: handle no data prediction

--- api/controllers/PredictionController.py
+++ api/controllers/PredictionController.py
@@ -9,6 +9,9 @@ class StockPricePredictionView(APIView):
 
         predictions = PredictionUseCase.predict_stock_price(symbol)
 
+        if not predictions:
+            return Response({"error": "Stock data not found"})
+
         PredictionUseCase.save_stock_prediction(symbol, predictions)
 
         return Response({"symbol": symbol, "predictions": predictions})
